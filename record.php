f773f59333f2ae74aac0df64a0dea095fe8107bd
danifabbro <la tua email>
2025-01-05 11:12:34 +0100

    corretta nomenclatura variabile

--- record.php
+++ record.php
@@ -4,13 +4,13 @@
     $password = "K@[3G/jHhajtVWN[";
     $dbname = "spesefamiliari";
 
-    $connessione = new mysqli($servername, $username, $password, $dbname);
+    $connessione = new mysqli($servername, $username, $password, $dbname); // creiamo una connessione al server con new mysqli 
 
-    if($conn === false){
+    if($connessione === false){
         die("Connessione fallita: " . $connessione->connect_error);
     }
 
-    echo "Connessione avvenuta con successo: ". $conn->host_info;  // $conn->host_info spiega da quale server arriva la connessione e quale tipo di protocollo si sta utilizzando. 
+    echo "Connessione avvenuta con successo: ". $connessione->host_info;  // $conn->host_info spiega da quale server arriva la connessione e quale tipo di protocollo si sta utilizzando. 
 //spiegazione codice: abbiamo specificato 4 parametri di connessione al database e li abbiamo inseriti nel parametro di mysqli 
 ?>
 
