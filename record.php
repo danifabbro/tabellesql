4d15ee0e8922b8536048780c496976409f597de4
danifabbro <la tua email>
2025-01-11 10:57:20 +0100

    inserito form

--- record.php
+++ record.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <?php
     $servername = "localhost"; 
     $username = "dani";
@@ -38,3 +48,5 @@ $connessione->close(); // chiudo la connessione
 
 ?>
 
+</body>
+</html>
